820caad91d59f7311623054366eace3689185b95
Jimin Park <pjm950@gmail.com>
2025-04-14 00:11:29 +0900

    Added preset to exports.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export type * from "./type";
 
 export * from "./pipe/index.js";
 export * from "./plugin/index.js";
+export * from "./preset/index.js";
 export * from "./prompt/index.js";
 
 export * from "./client.js";
